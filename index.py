5887231dab2c3b8f592c2a35bc38a4a31691d2d7
07RRJ <rasmusrudko07@gmail.com>
2024-12-04 12:01:28 +0100

    aödlsfja

---
+++ index.py
@@ -0,0 +1,41 @@
+import os
+import random
+
+os.system("cls")
+
+# list_1 = ["hello", 2]         #declare a list
+
+# for number in list:           #write all list items seperatly
+#     print(number)
+
+# list_2 = []
+# while True:
+#     os.system("cls")
+
+#     new_item_1 = input("add to the list\n")
+
+#     list_2.append(new_item_1)
+
+#     print(list_2)
+
+# candy_bar = []
+
+# while True:
+#     os.system("cls")
+#     for bar in candy_bar:
+#         print(bar)
+    
+#     print("\nVill ändra")
+#     psositon = input("vilken?")
+#     if psositon:
+#         new_name = input("skriiv nytt namn på chokladen")
+#         candy_bar[int(psositon)] = new_name
+
+
+
+
+# names = ["Rasmus", "Rudko", "Jensen"]
+# for name in names:
+#     print(f"{name}")
+
+random_items = ["Lorem", "ipsum", "dolor", "sit", "amet", "consectetur", "adipisicing", "elit.", "Aliquam", "quo", "natus", "cumque", "sit", "obcaecati", "repellat", "nisi"]
